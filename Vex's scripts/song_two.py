226c3d29c9c12605314bc93078a989afad046189
Brian Linkletter <mail@brianlinkletter.ca>
2023-02-26 21:35:28 -0500

    Added neopixel strip and music scripts

---
+++ Vex's scripts/song_two.py
@@ -0,0 +1,16 @@
+
+from adafruit_circuitplayground import cp
+
+cp.play_tone(300,0.5)
+cp.play_tone(500,1.75)
+cp.play_tone(450,0.3)
+cp.play_tone(400,1.75)
+cp.play_tone(300,0.5)
+cp.play_tone(595,1.75)
+cp.play_tone(525,0.3)
+cp.play_tone(500,2)
+cp.play_tone(660,0.2)
+cp.play_tone(660,0.5)
+cp.play_tone(595,0.9)
+cp.play_tone(595,0.5)
+cp.play_tone(750,1)
